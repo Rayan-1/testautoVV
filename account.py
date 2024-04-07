aabf0ab1e4433e7d282d2179b05835519832598b
Rayan-1 <rayanvictor088@gmail.com>
2024-04-07 13:14:10 -0300

    update

--- account.py
+++ account.py
@@ -2,11 +2,7 @@ import datetime
 import pytz  
 
 class Account:
-<<<<<<< HEAD
- def __init__(self, initial_balance=0, transaction_limit_hour=12):
-=======
- def __init__(self, initial_balance=0, transaction_limit_hour=13):
->>>>>>> 16bdc6b776b89e38c9e2f1b59df1f12e6ea5f77e
+ def __init__(self, initial_balance=0, transaction_limit_hour=16):
         self.balance = initial_balance
         self.transaction_limit_hour = transaction_limit_hour
 
@@ -24,8 +20,4 @@ class Account:
     self.bank_policy()
     if amount > self.balance:
         raise ValueError("Saldo insuficiente")
-<<<<<<< HEAD
     self.balance -= amount
-=======
-    self.balance -= amount
->>>>>>> 16bdc6b776b89e38c9e2f1b59df1f12e6ea5f77e
