80da71e3ec0a68479c706ca8290d9a6d8643e429
Rayan-1 <rayanvictor088@gmail.com>
2024-04-07 12:38:29 -0300

    update

--- test_account.py
+++ test_account.py
@@ -1,34 +1,24 @@
-import pytest
 import datetime
+import pytest
 from account import Account
 
-#Test Unitary
+# Test Unitary
 def test_deposit():
     account = Account(initial_balance=100)
     account.deposit(50)
     assert account.balance == 150
 
-#Test Unitary
+# Test Unitary
 def test_withdraw():
     account = Account(initial_balance=100)
     account.withdraw(50)
     assert account.balance == 50
 
-
-#Integration Test
+# Integration Test
 def test_deposit_and_withdraw():
     account = Account(initial_balance=100)
     account.deposit(50)
     account.withdraw(30)
-    assert account.balance == 50
+    assert account.balance == 120
 
-#Integration Test
-def bank_policy():
-    account = Account(initial_balance=100)
-    current_time = datetime.datetime.now()
 
-    with pytest.raises(ValueError):
-        account.deposit(50)
-
-    with pytest.raises(ValueError):
-        account.withdraw(30)
